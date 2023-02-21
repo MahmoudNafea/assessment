c2f5cb8f3baa804112d8b0fa6089295c823b2013
mahmoud <mahmouddeveloper19@gmail.com>
2023-02-21 21:02:01 +0800

    convert project from jsx to typescript

--- src/components/layout/card.tsx
+++ src/components/layout/card.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 
-const CardComponent=({post})=> {
+const CardComponent=({post}:any)=> {
   return (
     <Card style={{ width: '18rem' }}>
       <Card.Body>
-        <Card.Title>{post.title}</Card.Title>
+        <Card.Title>{post?.title}</Card.Title>
         <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
         <Card.Text>
         {post.summary}
